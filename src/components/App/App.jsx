a417f7dc7f9458f63e32524183789d7aca333273
Volodymyr180688 <shurik180688@gmail.com>
2024-05-11 18:34:20 +0300

    init project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'react'
+import axios from "axios";
+// import style from '../App/App.module.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
